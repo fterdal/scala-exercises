e63fe2318db0cc39d3bd20b582f3062242671ab5
Finn Terdal <finn@crowdbrowse.com>
2015-09-10 01:00:20 -0700

    Finished the uncurrying problem, and cleaned up the parentheses for ex-2.3

--- .ex2-3.scala
+++ .ex2-3.scala
@@ -9,7 +9,7 @@
 object MyModule {
 
   def curry[A,B,C](f: (A,B) => C): A => (B => C) = {
-    (a: A) => ((b: B) => f(a,b))
+    (a: A) => (b: B) => f(a,b)
   }
 
   def main(args: Array[String]): Unit = {
--- ex2-4.scala
+++ ex2-4.scala
@@ -2,14 +2,19 @@
  *  Author: Finn Terdal
  *  Date: September 10th, 2015
  *  This program provides a solution to exercise 2.3, which asks
- *  for an implementation to the currying function. There is only
+ *  for an implementation to the uncurrying function. There is only
  *  one solution that compiles, so this should be it.
  */
 
 object MyModule {
 
+  // Leftover from previous exercise
   def curry[A,B,C](f: (A,B) => C): A => (B => C) = {
-    (a: A) => ((b: B) => f(a,b))
+    (a: A) => (b: B) => f(a,b)
+  }
+
+  def uncurry[A,B,C](f: A => B => C): (A, B) => C = {
+    (a: A, b: B) => f(a)(b)
   }
 
   def main(args: Array[String]): Unit = {
