e5c5d9d7d046f4afc1fd7a98d383c89e2d1ad7e7
Finn Terdal <finn@crowdbrowse.com>
2015-09-10 00:52:12 -0700

    Hides ex-2.3 and starts ex-2.4

---
+++ ex2-4.scala
@@ -0,0 +1,18 @@
+/**
+ *  Author: Finn Terdal
+ *  Date: September 10th, 2015
+ *  This program provides a solution to exercise 2.3, which asks
+ *  for an implementation to the currying function. There is only
+ *  one solution that compiles, so this should be it.
+ */
+
+object MyModule {
+
+  def curry[A,B,C](f: (A,B) => C): A => (B => C) = {
+    (a: A) => ((b: B) => f(a,b))
+  }
+
+  def main(args: Array[String]): Unit = {
+    
+  }
+}
